38b1387ff37e05f8ff755cca100fda33191662f2
Alyssa Ross <hi@alyssa.is>
2016-04-24 18:54:48 +0100

    Log clicked links

--- app.rb
+++ app.rb
@@ -6,8 +6,9 @@ require "bundler"
 Bundler.require
 
 post "/message" do
-  body = Nokogiri::HTML params["body-html"] 
+  body = Nokogiri::HTML params["body-html"]
   if link = body.css("a").first
+    puts "Clicking link #{link.attr("href")}"
     Net::HTTP.get URI link.attr("href")
   end
 end
